3a2f3ca95ac65a88e82173438a69f626dbe3ed05
grosz99 <93449265+grosz99@users.noreply.github.com>
2023-12-31 10:21:39 -0500

    adding filters

--- kpi_dash.py
+++ kpi_dash.py
@@ -9,3 +9,13 @@ data = pd.read_csv(data_url)
 
 # Streamlit app setup
 st.title("KPI Dashboard")
+
+
+# Filters
+st.sidebar.header("Filters")
+selected_country = st.sidebar.selectbox("Country", data['Country'].unique())
+selected_city = st.sidebar.selectbox("City", data[data['Country'] == selected_country]['City'].unique())
+selected_state = st.sidebar.selectbox("State", data[data['City'] == selected_city]['State'].unique())
+
+# Filtered data based on user selection
+filtered_data = data[(data['Country'] == selected_country) & (data['City'] == selected_city) & (data['State'] == selected_state)]
