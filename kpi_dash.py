07ee60b48a5504ab49fb7e57e27f2cb36adf0a78
grosz99 <93449265+grosz99@users.noreply.github.com>
2023-12-31 10:10:19 -0500

    Update kpi_dash.py

--- kpi_dash.py
+++ kpi_dash.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 
 
+# Load the dataset
+data = pd.read_csv(r'C:\Users\grosz justin\OneDrive - The Boston Consulting Group, Inc\Documents\AI\Sample_Superstore_Streamlit_Proper.csv')
+
+
 
 # Streamlit app setup
 st.title("KPI Dashboard")
