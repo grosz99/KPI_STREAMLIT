fddcc7d6a895d76c9d79c04f40682e902a1dca52
grosz99 <93449265+grosz99@users.noreply.github.com>
2023-12-31 10:25:53 -0500

    working on filters

--- kpi_dash.py
+++ kpi_dash.py
@@ -14,8 +14,13 @@ st.title("KPI Dashboard")
 # Filters
 st.sidebar.header("Filters")
 selected_country = st.sidebar.selectbox("Country", data['Country'].unique())
-selected_states = st.sidebar.multiselect("State", data[data['Country'] == selected_country]['State'].unique())
-selected_cities = st.sidebar.multiselect("City", data[data['State'].isin(selected_states)]['City'].unique())
+
+# Default to all states and cities for the selected country
+default_states = data[data['Country'] == selected_country]['State'].unique()
+default_cities = data[data['Country'] == selected_country]['City'].unique()
+
+selected_states = st.sidebar.multiselect("State", default_states, default=default_states)
+selected_cities = st.sidebar.multiselect("City", default_cities, default=default_cities)
 
 # Filtered data based on user selection
 filtered_data = data[(data['Country'] == selected_country) & (data['State'].isin(selected_states)) & (data['City'].isin(selected_cities))]
