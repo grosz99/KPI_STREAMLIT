247349d1c2bb4fa3389f3c6b7c6ba82264afb2c7
grosz99 <93449265+grosz99@users.noreply.github.com>
2023-12-31 10:20:50 -0500

    correct how to get data

--- kpi_dash.py
+++ kpi_dash.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 
 # Load the dataset
-data = pd.read_csv(r'C:\Users\grosz justin\OneDrive - The Boston Consulting Group, Inc\Documents\AI\Sample_Superstore_Streamlit_Proper.csv')
-
+data_url = "https://raw.githubusercontent.com/grosz99/KPI_STREAMLIT/main/Sample_Superstore_Streamlit_Proper.csv"
+data = pd.read_csv(data_url)
 
 
 # Streamlit app setup
